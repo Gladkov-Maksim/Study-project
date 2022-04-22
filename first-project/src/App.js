b743538721e2d62679e6c5b0a425b819160aec66
Gladkov-Maksim <gladavikq@yandex.ru>
2022-04-22 20:08:37 +0300

    table

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import Paragraph from './components/paragraph.jsx'
 import Link from './components/link.jsx'
 import Image from './components/image.jsx'
+import Table from './components/table/table.jsx'
 
 function App() {
-
+  const headersArr = ['id', 'name', 'surname', 'age']
+  const tableContent = [{ id: 1, name: "Петр", surname: "Васичкин", age: 22 }, { id: 2, name: "Василий", surname: "Иванов", age: 12 }, { id: 3, name: "Иван", surname: "Сидоров", age: 32 }]
   return (
     <div className="App">
       <header className="App-header">
         <Image />
         <Paragraph>Edit <code>src/App.js</code> and save to reload.</Paragraph>
-        <Link to="https://reactjs.org"/>
+        <Link to="https://reactjs.org" />
+        <Table headers={headersArr} content={tableContent}/>
       </header>
     </div>
   );
