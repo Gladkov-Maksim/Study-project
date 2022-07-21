34ac3c35cd4a68bd52255c9acd33a2fe9893cd27
Gladkov-Maksim <gladavikq@yandex.ru>
2022-07-21 11:55:26 +0300

    video-time

--- first-project/src/components/Counter/ClassComponent.jsx
+++ first-project/src/components/Counter/ClassComponent.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error, errorInfo) {
+      // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+      console.log(error, errorInfo);
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        // Можно отрендерить запасной UI произвольного вида
+        return <h1>Что-то пошло не так.</h1>;
+      }
+  
+      return this.props.children; 
+    }
+  }
+
+const Number = ({number}) => {
+    console.log(number)
+    return number.currentNumber()
+}
+
 class ClassComponent extends React.Component {
     constructor (props){
         super(props)
         this.state = {currentNumber: 0}
     }
-    componentDidCatch(error, errorInfo) {
-        console.log(error)
-    }
-    static getDerivedStateFromError(error) {
-        console.log(error)
-    }
+    
     render () {
         return (
             <div>
@@ -18,16 +44,11 @@ class ClassComponent extends React.Component {
                 <button
                     onClick={() => this.setState({currentNumber: this.state.currentNumber - 1})}
                 >-</button>
-                <span>{this.state.currentNumber}</span>
+                <span><ErrorBoundary><Number number={this.state.currentNumber}/></ErrorBoundary></span>
                 <button
                     onClick={() => this.setState({currentNumber: this.state.currentNumber + 1})}
                 >+</button>
-                <button
-                    onClick={()=> {
-                        const x = 0
-                        x = 9
-                    }}
-                >error</button>
+                <button>error</button>
             </div>
             )
     }
---
+++ video-time/src/App3.js
@@ -0,0 +1,107 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+function YearTable(props) {
+    console.log('YearTable', props);
+
+    return (
+        <div>
+            <h2>Year Table</h2>
+            <table>
+                <tr>
+                    <th>Year</th>
+                    <th>Amount</th>
+                </tr>
+                {props.list.map(item => (
+                    <tr>
+                        <td>{item.year}</td>
+                        <td>{item.amount}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+};
+
+function yearHOC (Component) {
+    return function (props) {
+        const list = props.list.map(item => {
+            if (item.date) {
+                const year = item.date.slice(0, 4)
+                return {...item, year: year}
+            }
+        })
+        props.list = list
+        return Component.apply(null, [props])
+    }
+}
+const ModernYear = yearHOC(YearTable)
+
+function SortTable(props) {
+    console.log('SortTable', props);
+
+    return (
+        <div>
+            <h2>Sort Table</h2>
+            <table>
+                <tr>
+                    <th>Date</th>
+                    <th>Amount</th>
+                </tr>
+                {props.list.map(item => (
+                    <tr>
+                        <td>{item.date}</td>
+                        <td>{item.amount}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+};
+
+function MonthTable(props) {
+    console.log('MonthTable', props);
+
+    return (
+        <div>
+            <h2>Month Table</h2>
+            <table>
+                <tr>
+                    <th>Month</th>
+                    <th>Amount</th>
+                </tr>
+                {props.list.map(item => (
+                    <tr>
+                        <td>{item.month}</td>
+                        <td>{item.amount}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+};
+
+// TODO:
+// 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
+// 2. Не забудьте вынести URL в переменные окружения (не хардкодьте их здесь)
+// 3. Положите их в state
+export default function App () {
+    // state = {
+    //     list: []
+    // };
+    const [state, setState] = useState({ list: [] })
+    useEffect(() => {
+        axios.get('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json').then(res => {
+            setState(res.data)
+        })
+    }, [])
+        const {list} = state;
+    console.log(list, 111111111111111111111111111111111111111111111111111111111111)
+        return (
+            <div id="app">
+                <MonthTable list={list} />
+                <ModernYear list={list} />
+                <SortTable list={list} />
+            </div>
+        )
+}
