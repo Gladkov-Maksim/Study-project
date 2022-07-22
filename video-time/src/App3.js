e111e8c1430c5b89ff713b4d317df5420118f600
Gladkov-Maksim <gladavikq@yandex.ru>
2022-07-22 11:23:45 +0300

    video-time done

--- video-time/src/App3.js
+++ video-time/src/App3.js
@@ -25,13 +25,25 @@ function YearTable(props) {
 
 function yearHOC (Component) {
     return function (props) {
-        const list = props.list.map(item => {
+        const groupObj = {}
+        props.list.forEach(item => {
             if (item.date) {
                 const year = item.date.slice(0, 4)
-                return {...item, year: year}
+                if (groupObj[year]) {
+                    groupObj[year] += item.amount
+                }
+                else {
+                    groupObj[year] = item.amount
+                }
             }
         })
-        //props.list = list
+        const keys = Object.keys(groupObj)
+        const list = keys.map( key => {
+            const obj = {}
+            obj.year = key
+            obj.amount = groupObj[key]
+            return obj
+        })
         return Component.apply(null, [Object.assign({}, props, {list:list})])
     }
 }
@@ -59,6 +71,18 @@ function SortTable(props) {
     );
 };
 
+function sortHOC (Component) {
+    return function (props) {
+        const list = props.list
+        list.sort((a, b) => {
+            return Number(new Date(b.date)) - Number(new Date(a.date))
+        })
+        return Component.apply(null, [{...props, list: list}])
+    }
+}
+
+const ModernSort = sortHOC(SortTable)
+
 function MonthTable(props) {
     console.log('MonthTable', props);
 
@@ -81,6 +105,50 @@ function MonthTable(props) {
     );
 };
 
+function monthHOC (Component) {
+    return function (props) {
+        const groupObj = {}
+        props.list.forEach(item => {
+            const year = item.date.slice(0, 4)
+            if (year === '2018' && item.date) {
+                const month = item.date.slice(5, 7)
+                if (groupObj[month]) {
+                    groupObj[month] += item.amount
+                }
+                else {
+                    groupObj[month] = item.amount
+                }
+            }
+        })
+        const keys = Object.keys(groupObj)
+        keys.sort((a, b) => a - b)
+
+        const monthsNames = {'01':'Jan',
+                             '02': 'Feb',
+                             '03': 'Mar',
+                             '04': 'Apr',
+                             '05': 'May',
+                             '06': 'Jun',
+                             '07': 'Jul',
+                             '08': 'Aug',
+                             '09': 'Sep',
+                             '10': 'Okt',
+                             '11': 'Nov',
+                             '12': 'Dec'} // Есть ли каой-то вариант избежать этот колхоз
+
+
+        const list = keys.map( key => {
+            const obj = {}
+            obj.month = monthsNames[key]
+            obj.amount = groupObj[key]
+            return obj
+        })
+        return Component.apply(null, [Object.assign({}, props, {list: list})])
+    }
+}
+
+const ModernMonth = monthHOC(MonthTable)
+
 // TODO:
 // 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
 // 2. Не забудьте вынести URL в переменные окружения (не хардкодьте их здесь)
@@ -96,12 +164,11 @@ export default function App () {
         })
     }, [])
         const {list} = state;
-    console.log(list, 111111111111111111111111111111111111111111111111111111111111)
         return (
             <div id="app">
-                <MonthTable list={list} />
+                <ModernMonth list={list} />
                 <ModernYear list={list} />
-                <SortTable list={list} />
+                <ModernSort list={list} />
             </div>
         )
 }
